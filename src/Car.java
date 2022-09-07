7e3985c3c9c999b25608c42d357b723474ddf854
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-07 16:14:16 +0600

    a

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Car {
+public class Car implements AutoCloseable{
+
+        public void drive(){
+            System.out.println("===================================");
+            System.out.println("Машина журуп жатат");
+            System.out.println("===================================");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("===================================");
+        System.out.println("Машина жабылып жатат");
+        System.out.println("===================================");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+            try (Car car = new Car()) {
+                car.drive();
+          } catch (RuntimeException e) {
+            System.out.println(e);
+            }catch (Exception e){
+            throw new RuntimeException();
+         }
+        }
+      }
